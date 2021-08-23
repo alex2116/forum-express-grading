f7650309c2b66f426b492b2e20aae95fe3d506e8
alex2116 <reingeld116@gmail.com>
2021-08-24 00:44:45 +0800

    install passport and passport-local & add featureL singinPage, signIn and logout

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const db = require('./models')
 const bodyParser = require('body-parser')
 const flash = require('connect-flash')
 const session = require('express-session')
+const passport = require('./config/passport')
 const { sequelize } = require('./models')
 
 const app = express()
@@ -13,6 +14,8 @@ app.engine('handlebars', handlebars({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(session({secret: 'secret', resave: false, saveUninitialized: false}))
+app.use(passport.initialize())
+app.use(passport.session())
 app.use(flash())
 
 app.use((req, res, next) => {
@@ -26,6 +29,6 @@ app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
 
-require('./routes')(app)
+require('./routes')(app, passport)
 
 module.exports = app
