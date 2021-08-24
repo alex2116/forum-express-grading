54ea837be9af7730e7a94a2ee4fcdecb84b749ad
alex2116 <reingeld116@gmail.com>
2021-08-24 19:55:10 +0800

    add image to restaurant & install multer and add image upload feature

--- controllers/adminController.js
+++ controllers/adminController.js
@@ -1,3 +1,4 @@
+const fs = require('fs')
 const db = require('../models')
 const Restaurant = db.Restaurant
 
@@ -18,17 +19,37 @@ let adminController = {
       return res.redirect('back')
     }
 
-    return Restaurant.create({
-      name: req.body.name,
-      tel: req.body.tel,
-      address: req.body.address,
-      opening_hours: req.body.opening_hours,
-      description: req.body.description
-    })
-    .then((restaurant) => {
-      req.flash('success_messages', 'restaurant was successfully created')
-      res.redirect('/admin/restaurants')
-    })
+    const { file } = req
+    if (file) {
+      fs.readFile(file.path, (err, data) => {
+        if (err) console.log('Error: ', err)
+        fs.writeFile(`upload/${file.originalname}`, data, () => {
+          return Restaurant.create({
+            name: req.body.name,
+            tel: req.body.tel,
+            address: req.body.address,
+            opening_hours: req.body.opening_hours,
+            description: req.body.description,
+            image: file ? `/upload/${file.originalname}` : null,
+          }).then((restaurant) => {
+            req.flash('success_messages', 'restaurant was successfully created')
+            return res.redirect('/admin/restaurants')
+          })
+        })
+      })
+    } else {
+      return Restaurant.create({
+        name: req.body.name,
+        tel: req.body.tel,
+        address: req.body.address,
+        opening_hours: req.body.opening_hours,
+        description: req.body.description,
+        image: null
+      }).then((restaurant) => {
+        req.flash('success_messages', 'restaurant was successfully created')
+        res.redirect('/admin/restaurants')
+      })
+    }    
   },
 
   getRestaurant: (req, res) => {
@@ -49,20 +70,44 @@ let adminController = {
       return res.redirect('back')
     }
 
-    return Restaurant.findByPk(req.params.id)
+    const { file } = req
+    if (file) {
+      fs.readFile(file.path, (err, data) => {
+        if (err) console.log('Error: ', err)
+        fs.writeFile(`upload/${file.originalname}`, data, () => {
+          return Restaurant.findByPk(req.params.id)
+            .then((restaurant) => {
+              restaurant.update({
+                name: req.body.name,
+                tel: req.body.tel,
+                address: req.body.address,
+                opening_hours: req.body.opening_hours,
+                description: req.body.description,
+                image: file ? `/upload/${file.originalname}` : restaurant.image
+              }).then((restaurant) => {
+                req.flash('success_messages', 'restaurant was successfully to update')
+                res.redirect('/admin/restaurants')
+              })
+            })
+        })
+      })
+    } else {
+      return Restaurant.findByPk(req.params.id)
       .then((restaurant) => {
         restaurant.update({
           name: req.body.name,
           tel: req.body.tel,
           address: req.body.address,
           opening_hours: req.body.opening_hours,
-          description: req.body.description
+          description: req.body.description,
+          image: restaurant.image
         })
         .then((restaurant) => {
           req.flash('success_messages', 'restaurant has successfully updated')
           res.redirect('/admin/restaurants')
         })
       })
+    }    
   },
 
   deleteRestaurant: (req, res) => {
