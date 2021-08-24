ef94abf3bd3cdfb614f6a17093bab09c9e134e1f
alex2116 <reingeld116@gmail.com>
2021-08-24 17:03:13 +0800

    update admin create page for editing usage & install method-override & add admin restaurant put feature

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ const flash = require('connect-flash')
 const session = require('express-session')
 const passport = require('./config/passport')
 const { sequelize } = require('./models')
+const methodOverride = require('method-override')
 
 const app = express()
 const port = 3000
@@ -17,6 +18,7 @@ app.use(session({secret: 'secret', resave: false, saveUninitialized: false}))
 app.use(passport.initialize())
 app.use(passport.session())
 app.use(flash())
+app.use(methodOverride('_method'))
 
 app.use((req, res, next) => {
   res.locals.success_messages = req.flash('success_messages')
--- routes/index.js
+++ routes/index.js
@@ -27,6 +27,8 @@ module.exports = (app, passport) => {
   app.get('/admin/restaurants/create', authenticatedAdmin, adminController.createRestaurant)
   app.post('/admin/restaurants', authenticatedAdmin, adminController.postRestaurant)
   app.get('/admin/restaurants/:id', authenticatedAdmin, adminController.getRestaurant)
+  app.get('/admin/restaurants/:id/edit', authenticatedAdmin, adminController.editRestaurant)
+  app.put('/admin/restaurants/:id', authenticatedAdmin, adminController.putRestaurant)
 
   app.get('/signup', userController.signUpPage)
   app.post('/signup', userController.signUp)
@@ -34,6 +36,5 @@ module.exports = (app, passport) => {
   app.get('/signin', userController.signInPage)
   app.post('/signin', passport.authenticate('local', { failureRedirect: '/signin', failureFlash: true }), userController.signIn)
   app.get('/logout', userController.logout)
-
   
 }
