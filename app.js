5bb95e6f549e43b353b168b1ebe146131c8e31ee
alex2116 <reingeld116@gmail.com>
2021-08-25 09:12:57 +0800

    Listen to process.env.Port & define Procfile

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const { sequelize } = require('./models')
 const methodOverride = require('method-override')
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.engine('handlebars', handlebars({ defaultLayout: 'main' }))
 app.set('view engine', 'handlebars')
