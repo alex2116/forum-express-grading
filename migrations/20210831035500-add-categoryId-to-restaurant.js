d49384308373c1d0840e7ae6177a85107d655740
alex2116 <reingeld116@gmail.com>
2021-08-31 14:54:26 +0800

    add category seed data

--- migrations/20210831035500-add-categoryId-to-restaurant.js
+++ migrations/20210831035500-add-categoryId-to-restaurant.js
@@ -1,34 +1,18 @@
-'use strict';
+'use strict'
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
     await queryInterface.addColumn('Restaurants', 'CategoryId', {
       type: Sequelize.INTEGER,
       allowNull: false,
-    });
-
-    // 維護舊資料
-    await queryInterface.bulkUpdate('Restaurants',
-      {
-        CategoryId: 1, // 預設為 1, 假設 Categories 有 id 1 的資料
-      },
-      {}
-    );
-
-    // 加 Constraint
-    await queryInterface.addConstraint('Restaurants', {
-      fields: ['CategoryId'],
-      type: 'foreign key',
       references: {
-        table: 'Categories',
-        field: 'id'
-      },
-      onDelete: 'cascade', // 這自行學習是做什麼的
-      onUpdate: 'cascade'  // 這自行學習是做什麼的
-    });
+        model: 'Categories',
+        key: 'id'
+      }
+    })
   },
 
   down: async (queryInterface, Sequelize) => {
     await queryInterface.removeColumn('Restaurants', 'CategoryId')
   }
-};
+}
