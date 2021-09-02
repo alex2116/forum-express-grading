3865b4a1ae5aab891cc4f1f02a81b3f9119fee4b
alex2116 <reingeld116@gmail.com>
2021-09-02 16:07:46 +0800

    Create comment model & add association among users, comment, restaurants

--- controllers/restController.js
+++ controllers/restController.js
@@ -4,34 +4,50 @@ const { getCategories } = require('./categoryController')
 const Restaurant = db.Restaurant
 const Category = db.Category
 
+const pageLimit = 10
+
 const restController = {
   getRestaurants: (req, res) => {
     const whereQuery = {}
     let categoryId = ''
+    let offset = 0
+    if (req.query.page) {
+      offset = (req.query.page - 1) * pageLimit
+    }
     if (req.query.categoryId) {
       categoryId = Number(req.query.categoryId) 
       whereQuery.CategoryId = categoryId
     }
-     Restaurant.findAll({        
+     Restaurant.findAndCountAll({               
        include: Category,
-       where: whereQuery
-       }).then(restaurants => {
-      const data = restaurants.map(r => ({
-        ...r.dataValues,
-        description: r.dataValues.description.substring(0, 50),
-        categoryName: r.Category.name
-      }))      
-      Category.findAll({
-        raw: true,
-        nest: true
-      }).then(categories => {
-        return res.render('restaurants', {
-          restaurants: data,
-          categories,
-          categoryId
-        })
-      })
-      
+       where: whereQuery, //必須傳入物件
+       offset,
+       limit: pageLimit
+     }).then(result => {
+       const page = Number(req.query.page) || 1
+       const pages = Math.ceil(result.count / pageLimit)
+       const totalPage = Array.from({ length: pages }).map((item, index) => index + 1)
+       const prev = page - 1 < 1 ? 1 : page - 1
+       const next = page + 1 > pages ? pages : page + 1
+       const data = result.rows.map(r => ({
+         ...r.dataValues,
+         description: r.dataValues.description.substring(0, 50),
+         categoryName: r.dataValues.Category.name
+       }))      
+       Category.findAll({
+         raw: true,
+         nest: true
+       }).then(categories => {
+         return res.render('restaurants', {
+           restaurants: data,
+           categories,
+           page,
+           totalPage,
+           prev,
+           next,
+           categoryId
+         })
+       })         
     })
   },
 
